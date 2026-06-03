c942089917c639e4a9623fd74df097002b08d0ed
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-03 16:53:23 -0400

    Fix JEI compatibility for brewing recipes with NBT data
    This update enhances JEI compatibility by ensuring that potion recipes with NBT data are correctly recognized. Adjusted the input and ingredient checks to handle NBT tags properly.

--- src/main/java/com/charles445/simpledifficulty/register/crafting/FixedBrewingOreRecipe.java
+++ src/main/java/com/charles445/simpledifficulty/register/crafting/FixedBrewingOreRecipe.java
@@ -6,25 +6,30 @@ import net.minecraftforge.oredict.OreDictionary;
 
 import javax.annotation.Nonnull;
 
+/**
+ * Fixed brewing recipe that properly handles NBT data for JEI compatibility.
+ * This ensures that potion recipes with NBT data are correctly recognized by JEI.
+ */
 public class FixedBrewingOreRecipe extends AbstractBrewingRecipe<ItemStack>
 {
-	//I wasn't expecting this to actually be a problem but it is
-	//BrewingRecipe and BrewingOreRecipe are set up to handle OreDict items well, but not anything NBT related
-	
-	public FixedBrewingOreRecipe(ItemStack input, ItemStack ingredient, ItemStack output)
-	{
-		super(input, ingredient, output);
-	}
+    // This fixes JEI detection issues with potion recipes containing NBT data
+    // Standard BrewingRecipe and BrewingOreRecipe don't handle NBT properly
+    
+    public FixedBrewingOreRecipe(ItemStack input, ItemStack ingredient, ItemStack output)
+    {
+        super(input, ingredient, output);
+    }
 
-	@Override
-	public boolean isInput(@Nonnull ItemStack stack)
-	{
-		return super.isInput(stack) && ItemStack.areItemStackTagsEqual(getInput(), stack);
-	}
+    @Override
+    public boolean isInput(@Nonnull ItemStack stack)
+    {
+        // Ensure NBT tags match exactly for proper JEI recognition
+        return super.isInput(stack) && ItemStack.areItemStackTagsEqual(getInput(), stack);
+    }
 
-	@Override
-	public boolean isIngredient(ItemStack ingredient)
-	{
-		return OreDictionary.itemMatches(this.getIngredient(), ingredient, false);
-	}
+    @Override
+    public boolean isIngredient(ItemStack ingredient)
+    {
+        return OreDictionary.itemMatches(this.getIngredient(), ingredient, false);
+    }
 }
