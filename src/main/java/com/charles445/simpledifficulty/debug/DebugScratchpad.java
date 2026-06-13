b0884cf00c6699763c146cde03fbe34f5085e52e
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-12 22:24:51 -0400

    Refactor DebugScratchpad with explanatory comments
    Updated comments to clarify the purpose of the DebugScratchpad class.

--- src/main/java/com/charles445/simpledifficulty/debug/DebugScratchpad.java
+++ src/main/java/com/charles445/simpledifficulty/debug/DebugScratchpad.java
@@ -1,29 +1,28 @@
 package com.charles445.simpledifficulty.debug;
 
-public class DebugScratchpad
-{
-	/*
-	public static DebugScratchpad instance = new DebugScratchpad();
-	
-	public void init()
-	{
-		System.out.println(disableBuiltInModCompatibility("modid_goes_here"));
-	}
-	
-	//preInit or init
-	public boolean disableBuiltInModCompatibility(String modid)
-	{
-		try
-		{
-			Class SDCompatibility = Class.forName("com.charles445.simpledifficulty.api.SDCompatibility");
-			Method disableBuiltInModCompatibility = SDCompatibility.getDeclaredMethod("disableBuiltInModCompatibility", String.class);
-			disableBuiltInModCompatibility.invoke(null, modid);
-			return true;
-		}
-		catch(Exception e)
-		{
-			return false;
-		}
-	}
-	*/
+/**
+ * Basically, this was a temporary notepad file that the original author (charles445) 
+ * used to test how other mod developers could disable built-in support via reflection, 
+ * without having to compile against the API directly.
+ */
+public class DebugScratchpad {
+    /*
+    public static DebugScratchpad instance = new DebugScratchpad();
+    
+    public void init() {
+        System.out.println(disableBuiltInModCompatibility("modid_goes_here"));
+    }
+    
+    // preInit or init
+    public boolean disableBuiltInModCompatibility(String modid) {
+        try {
+            Class<?> sdCompatibility = Class.forName("com.charles445.simpledifficulty.api.SDCompatibility");
+            java.lang.reflect.Method disableBuiltInModCompatibility = sdCompatibility.getDeclaredMethod("disableBuiltInModCompatibility", String.class);
+            disableBuiltInModCompatibility.invoke(null, modid);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    */
 }
