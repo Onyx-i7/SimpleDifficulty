4c2e0dbe0078949f887a1b0f7e104367aaf73f39
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-12 22:04:17 -0400

    Refactor CoreLoader for null safety and clarity
    Refactor CoreLoader methods to use @Nullable annotation and improve null checks.

--- src/main/java/com/charles445/simpledifficulty/asm/CoreLoader.java
+++ src/main/java/com/charles445/simpledifficulty/asm/CoreLoader.java
@@ -3,43 +3,43 @@ package com.charles445.simpledifficulty.asm;
 import com.charles445.simpledifficulty.asm.helper.ObfHelper;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
+import javax.annotation.Nullable;
 import java.util.Map;
 
 @IFMLLoadingPlugin.Name("SimpleDifficulty ASM")
 @IFMLLoadingPlugin.SortingIndex(1001)
-@IFMLLoadingPlugin.TransformerExclusions({ "com.charles445.simpledifficulty.asm", "com.charles445.simpledifficulty.asm." })
-public class CoreLoader implements IFMLLoadingPlugin
-{
+@IFMLLoadingPlugin.TransformerExclusions({"com.charles445.simpledifficulty.asm"})
+public class CoreLoader implements IFMLLoadingPlugin {
 
-	@Override
-	public String[] getASMTransformerClass()
-	{
-		return new String[] { "com.charles445.simpledifficulty.asm.SimpleDifficultyASM" };
-	}
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[]{"com.charles445.simpledifficulty.asm.SimpleDifficultyASM"};
+    }
 
-	@Override
-	public String getModContainerClass()
-	{
-		return null;
-	}
+    @Nullable
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
 
-	@Override
-	public String getSetupClass()
-	{
-		return null;
-	}
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
 
-	@Override
-	public void injectData(Map<String, Object> data)
-	{
-		ObfHelper.setObfuscated((Boolean) data.get("runtimeDeobfuscationEnabled"));
-		ObfHelper.setRunsAfterDeobfRemapper(true);
-	}
+    @Override
+    public void injectData(Map<String, Object> data) {
+        if (data.containsKey("runtimeDeobfuscationEnabled")) {
+            Boolean isObfuscated = (Boolean) data.get("runtimeDeobfuscationEnabled");
+            ObfHelper.setObfuscated(isObfuscated != null && isObfuscated);
+        }
+        ObfHelper.setRunsAfterDeobfRemapper(true);
+    }
 
-	@Override
-	public String getAccessTransformerClass()
-	{
-		return null;
-	}
-	
+    @Nullable
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
 }
