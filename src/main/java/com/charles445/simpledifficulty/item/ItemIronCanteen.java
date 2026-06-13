dcdfbbeee206d9b649c74e5710d9698ed023fc05
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-13 18:01:34 -0400

    Refactor ItemIronCanteen for efficiency, clarity and Optimized

--- src/main/java/com/charles445/simpledifficulty/item/ItemIronCanteen.java
+++ src/main/java/com/charles445/simpledifficulty/item/ItemIronCanteen.java
@@ -5,54 +5,42 @@ import com.charles445.simpledifficulty.api.config.ServerConfig;
 import com.charles445.simpledifficulty.api.config.ServerOptions;
 import com.charles445.simpledifficulty.api.item.IItemCanteen;
 import com.charles445.simpledifficulty.api.thirst.ThirstEnum;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemIronCanteen extends ItemCanteen
-{
-	public ItemIronCanteen()
-	{
-		super();
-		
-		addPropertyOverride(new ResourceLocation("contain"), new IItemPropertyGetter()
-		{
-			@Override
-			@SideOnly(Side.CLIENT)
-			public float apply(ItemStack stack, World worldIn, EntityLivingBase entityIn)
-			{
-				if(stack.getItem() instanceof IItemCanteen)
-				{
-					IItemCanteen canteen = (IItemCanteen)stack.getItem();
-					if(!canteen.isCanteenEmpty(stack))
-						return 1.0f;
-				}
-				
-				return 0.0f;
-			}
-		});
-	}
-	
-	@Override
-	public int getMaxDoses(ItemStack stack)
-	{
-		return ServerConfig.instance.getInteger(ServerOptions.IRON_CANTEEN_DOSES);
-	}
-	
-	@Override
-	public String getTranslationKey(ItemStack stack)
-	{
-		if(isCanteenEmpty(stack))
-			return "item."+SimpleDifficulty.MODID+":"+"iron_canteen_empty";
-		
-		int type = getTypeTag(stack).getInt();
-		if(type>=ThirstEnum.values().length)
-			return "item."+SimpleDifficulty.MODID+":"+"iron_canteen_broken";
-		
-		return "item."+SimpleDifficulty.MODID+":"+"iron_canteen_"+ThirstEnum.values()[type].toString();
-	}
+public class ItemIronCanteen extends ItemCanteen {
+
+    // Cached values array locally to prevent overhead from continuous values() cloning
+    private static final ThirstEnum[] THIRST_VALUES = ThirstEnum.values();
+
+    public ItemIronCanteen() {
+        super();
+        
+        addPropertyOverride(new ResourceLocation("contain"), (stack, worldIn, entityIn) -> {
+            if (stack.getItem() instanceof IItemCanteen) {
+                IItemCanteen canteen = (IItemCanteen) stack.getItem();
+                return !canteen.isCanteenEmpty(stack) ? 1.0f : 0.0f;
+            }
+            return 0.0f;
+        });
+    }
+    
+    @Override
+    public int getMaxDoses(ItemStack stack) {
+        return ServerConfig.instance.getInteger(ServerOptions.IRON_CANTEEN_DOSES);
+    }
+    
+    @Override
+    public String getTranslationKey(ItemStack stack) {
+        if (isCanteenEmpty(stack)) {
+            return "item." + SimpleDifficulty.MODID + ":iron_canteen_empty";
+        }
+        
+        int type = getTypeTag(stack).getInt();
+        if (type >= THIRST_VALUES.length || type < 0) {
+            return "item." + SimpleDifficulty.MODID + ":iron_canteen_broken";
+        }
+        
+        return "item." + SimpleDifficulty.MODID + ":iron_canteen_" + THIRST_VALUES[type].toString();
+    }
 }
