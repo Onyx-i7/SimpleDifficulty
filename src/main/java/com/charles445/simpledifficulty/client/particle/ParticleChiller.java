947aa27feb36ef776deae7e812f72ca2bba1ded6
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-12 21:26:56 -0400

    Refactor ParticleChiller for optimized particle age
    Refactored ParticleChiller constructor to use inherited random instance for particle age initialization.

--- src/main/java/com/charles445/simpledifficulty/client/particle/ParticleChiller.java
+++ src/main/java/com/charles445/simpledifficulty/client/particle/ParticleChiller.java
@@ -2,25 +2,22 @@ package com.charles445.simpledifficulty.client.particle;
 
 import net.minecraft.world.World;
 
-public class ParticleChiller extends ParticleTemperature
-{
-	public ParticleChiller(World world, double xPos, double yPos, double zPos, double motionX, double motionY, double motionZ)
-	{
-		super(world, xPos, yPos, zPos, motionX, motionY, motionZ);
-		
-		this.particleAge = world.rand.nextInt(2);
-	}
+public class ParticleChiller extends ParticleTemperature {
 
-	@Override
-	int getFrameMax()
-	{
-		return 8;
-	}
+    public ParticleChiller(World world, double xPos, double yPos, double zPos, double motionX, double motionY, double motionZ) {
+        super(world, xPos, yPos, zPos, motionX, motionY, motionZ);
+        
+        // Optimized: Use the internal inherited random instance for particle initialization consistency
+        this.particleAge = this.rand.nextInt(2);
+    }
 
-	@Override
-	int getParticleTextureY()
-	{
-		return 1;
-	}
-	
+    @Override
+    int getFrameMax() {
+        return 8;
+    }
+
+    @Override
+    int getParticleTextureY() {
+        return 1;
+    }
 }
