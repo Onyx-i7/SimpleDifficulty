94fa319b92971406d90742464a5f1ac4f826bbbb
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-12 21:02:41 -0400

    Refactor PacketHandler to use dynamic message IDs
    Refactored message registration to use automatic incremental IDs.

--- src/main/java/com/charles445/simpledifficulty/network/PacketHandler.java
+++ src/main/java/com/charles445/simpledifficulty/network/PacketHandler.java
@@ -5,17 +5,17 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
-public class PacketHandler 
-{
-	public static final SimpleNetworkWrapper instance = NetworkRegistry.INSTANCE.newSimpleChannel(SimpleDifficulty.MODID);
-	
-	public static void init()
-	{
-		instance.registerMessage(MessageUpdateThirst.Handler.class, MessageUpdateThirst.class, 0, Side.CLIENT);
-		instance.registerMessage(MessageDrinkWater.Handler.class, MessageDrinkWater.class, 1, Side.SERVER);
-		instance.registerMessage(MessageConfigLAN.Handler.class, MessageConfigLAN.class, 2, Side.SERVER);
-		instance.registerMessage(MessageUpdateConfig.Handler.class, MessageUpdateConfig.class, 3, Side.CLIENT);
-		instance.registerMessage(MessageUpdateTemperature.Handler.class, MessageUpdateTemperature.class, 4, Side.CLIENT);
-	}
-	
+public class PacketHandler {
+
+    public static final SimpleNetworkWrapper instance = NetworkRegistry.INSTANCE.newSimpleChannel(SimpleDifficulty.MODID);
+    
+    public static void init() {
+        int id = 0; // Automatic dynamic incremental registration
+        
+        instance.registerMessage(MessageUpdateThirst.Handler.class, MessageUpdateThirst.class, id++, Side.CLIENT);
+        instance.registerMessage(MessageDrinkWater.Handler.class, MessageDrinkWater.class, id++, Side.SERVER);
+        instance.registerMessage(MessageConfigLAN.Handler.class, MessageConfigLAN.class, id++, Side.SERVER);
+        instance.registerMessage(MessageUpdateConfig.Handler.class, MessageUpdateConfig.class, id++, Side.CLIENT);
+        instance.registerMessage(MessageUpdateTemperature.Handler.class, MessageUpdateTemperature.class, id++, Side.CLIENT);
+    }
 }
