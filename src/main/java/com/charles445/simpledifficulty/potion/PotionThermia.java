6236667d44731a3dbd2fde6befd4e38eb19352d9
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-12 21:51:35 -0400

    Refactor PotionThermia for safety and optimization
    Refactored PotionThermia class to include safety checks and optimize math operations.

--- src/main/java/com/charles445/simpledifficulty/potion/PotionThermia.java
+++ src/main/java/com/charles445/simpledifficulty/potion/PotionThermia.java
@@ -1,7 +1,6 @@
 package com.charles445.simpledifficulty.potion;
 
 import com.charles445.simpledifficulty.api.SDCapabilities;
-import com.charles445.simpledifficulty.api.SDPotions;
 import com.charles445.simpledifficulty.api.temperature.ITemperatureCapability;
 import com.charles445.simpledifficulty.config.ModConfig;
 import com.charles445.simpledifficulty.util.DamageUtil;
@@ -9,29 +8,35 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
-public abstract class PotionThermia extends PotionBase
-{	
-	public PotionThermia(boolean isBadEffect, int liquidColor)
-	{
-		super(isBadEffect, liquidColor);
-	}
+public abstract class PotionThermia extends PotionBase {    
 
-	@Override
-	public void performEffect(EntityLivingBase entity, int amplifier)
-	{
-		if(entity instanceof EntityPlayer && !entity.isPotionActive(SDPotions.heat_resist))
-		{
-			World world = entity.getEntityWorld();
-			EntityPlayer player = (EntityPlayer) entity;
-			if(DamageUtil.isModDangerous(world) && DamageUtil.healthAboveDifficulty(world, player))
-			{
-				ITemperatureCapability capability = SDCapabilities.getTemperatureData(player);
-				float damage = 0.5f + (0.5f * (float)capability.getTemperatureDamageCounter() * (float)ModConfig.server.temperature.temperatureDamageScaling);
-				attackPlayer(player, damage, amplifier);
-				capability.addTemperatureDamageCounter(1);
-			}
-		}
-	}
-	
-	public abstract void attackPlayer(EntityPlayer player, float damage, int amplifier);
+    public PotionThermia(boolean isBadEffect, int liquidColor) {
+        super(isBadEffect, liquidColor);
+    }
+
+    @Override
+    public void performEffect(EntityLivingBase entity, int amplifier) {
+        if (entity instanceof EntityPlayer) {
+            World world = entity.getEntityWorld();
+            EntityPlayer player = (EntityPlayer) entity;
+            
+            if (DamageUtil.isModDangerous(world) && DamageUtil.healthAboveDifficulty(world, player)) {
+                ITemperatureCapability capability = SDCapabilities.getTemperatureData(player);
+                
+                // Safety Check: Prevent crashes if capability data is temporarily missing or detached
+                if (capability == null) {
+                    return;
+                }
+                
+                // Math Optimization: Cleaned up redundant float casting on configuration constants
+                float scaling = (float) ModConfig.server.temperature.temperatureDamageScaling;
+                float damage = 0.5F + (0.5F * capability.getTemperatureDamageCounter() * scaling);
+                
+                attackPlayer(player, damage, amplifier);
+                capability.addTemperatureDamageCounter(1);
+            }
+        }
+    }
+    
+    public abstract void attackPlayer(EntityPlayer player, float damage, int amplifier);
 }
