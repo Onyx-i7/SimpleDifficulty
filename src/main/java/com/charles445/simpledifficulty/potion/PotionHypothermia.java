b7e2a0cefed5001e8fa3196625a636b0f7d48a32
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-12 21:44:40 -0400

    Refactor PotionHypothermia attackPlayer method
    Added performance and math optimizations to attackPlayer method.

--- src/main/java/com/charles445/simpledifficulty/potion/PotionHypothermia.java
+++ src/main/java/com/charles445/simpledifficulty/potion/PotionHypothermia.java
@@ -5,41 +5,41 @@ import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
 
-public class PotionHypothermia extends PotionThermia
-{
-	private final ResourceLocation texture;
-	
-	public PotionHypothermia()
-	{
-		super(true, 0x5CEBFF);
-		xOffset = 0;
-		yOffset = 0;
-		texture = formatTexture("hypothermia");
-	}
-	
-	@Override
-	public ResourceLocation getTexture()
-	{
-		return texture;
-	}
+public class PotionHypothermia extends PotionThermia {
 
-	@Override
-	public void attackPlayer(EntityPlayer player, float damage, int amplifier)
-	{
-		if (amplifier >= 4 && amplifier < 6)
-		{
-			player.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 405, 0));
-			player.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 405, 0));
-		}
-		else if (amplifier >= 6 && amplifier < 8)
-		{
-			player.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 405, 1));
-			player.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 405, 1));
-		}
-		else if (amplifier >= 8)
-		{
-			player.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 405, 2));
-			player.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 405, 2));
-		}
-	}
+    private final ResourceLocation texture;
+    
+    public PotionHypothermia() {
+        super(true, 0x5CEBFF);
+        this.xOffset = 0;
+        this.yOffset = 0;
+        this.texture = formatTexture("hypothermia");
+    }
+    
+    @Override
+    public ResourceLocation getTexture() {
+        return texture;
+    }
+
+    @Override
+    public void attackPlayer(EntityPlayer player, float damage, int amplifier) {
+        if (player == null || amplifier < 4) {
+            return;
+        }
+
+        // Performance Optimization: Check if effects are already active with significant remaining duration to block network spam
+        PotionEffect activeSlowness = player.getActivePotionEffect(MobEffects.SLOWNESS);
+        PotionEffect activeWeakness = player.getActivePotionEffect(MobEffects.WEAKNESS);
+        
+        if (activeSlowness != null && activeSlowness.getDuration() > 100 &&
+            activeWeakness != null && activeWeakness.getDuration() > 100) {
+            return;
+        }
+
+        // Math Optimization: Map dynamic amplifier tiers (4-5 -> 0, 6-7 -> 1, 8+ -> 2)
+        int effectAmplifier = Math.min((amplifier - 4) / 2, 2);
+
+        player.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 405, effectAmplifier));
+        player.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 405, effectAmplifier));
+    }
 }
