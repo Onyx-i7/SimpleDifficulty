e83e694eab851f5462c8273fa99384b122facfae
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-13 17:51:54 -0400

    Refactor ItemArmorTemperature class formatting

--- src/main/java/com/charles445/simpledifficulty/item/ItemArmorTemperature.java
+++ src/main/java/com/charles445/simpledifficulty/item/ItemArmorTemperature.java
@@ -4,18 +4,14 @@ import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 
-public class ItemArmorTemperature extends ItemArmor
-{
-	public ItemArmorTemperature(ArmorMaterial material, EntityEquipmentSlot equipmentSlot)
-	{
-		//TODO what does render index do? Is that the old render passes system?
-		
-		super(material, 0, equipmentSlot);
-	}
-	
-	@Override
-	public boolean hasOverlay(ItemStack stack)
-	{
-		return false;
-	}
+public class ItemArmorTemperature extends ItemArmor {
+
+    public ItemArmorTemperature(ArmorMaterial material, EntityEquipmentSlot equipmentSlot) {
+        super(material, 0, equipmentSlot);
+    }
+    
+    @Override
+    public boolean hasOverlay(ItemStack stack) {
+        return false;
+    }
 }
