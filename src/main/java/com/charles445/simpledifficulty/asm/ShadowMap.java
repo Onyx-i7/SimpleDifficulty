3d14bce332aa4b085b6ca1cc8d77cbf6437d28ec
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-12 22:06:15 -0400

    Refactor ShadowMap to use final static maps
    Refactored ShadowMap class to use final static maps and improved formatting.

--- src/main/java/com/charles445/simpledifficulty/asm/ShadowMap.java
+++ src/main/java/com/charles445/simpledifficulty/asm/ShadowMap.java
@@ -3,24 +3,25 @@ package com.charles445.simpledifficulty.asm;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ShadowMap
-{
-	public static Map<String, String> ownerMap;
-	public static Map<String, String> descMap;
-	
-	public static void addClassShadow(String shadow, String destination)
-	{
-		ownerMap.put(shadow, destination);
-		descMap.put("L"+shadow+";", "L"+destination+";");
-	}
-	
-	static
-	{
-		ownerMap = new ConcurrentHashMap<>();
-		descMap = new ConcurrentHashMap<>();
-		
-		//Inspirations
-		addClassShadow("com/charles445/simpledifficulty/compat/shadow/InspirationsShadow$ICauldronRecipe", "knightminer/inspirations/library/recipe/cauldron/ICauldronRecipe");
-		addClassShadow("com/charles445/simpledifficulty/compat/shadow/InspirationsShadow$ICauldronRecipe$CauldronState", "knightminer/inspirations/library/recipe/cauldron/ICauldronRecipe$CauldronState");
-	}
+public class ShadowMap {
+    
+    public static final Map<String, String> ownerMap = new ConcurrentHashMap<>();
+    public static final Map<String, String> descMap = new ConcurrentHashMap<>();
+    
+    public static void addClassShadow(String shadow, String destination) {
+        ownerMap.put(shadow, destination);
+        descMap.put("L" + shadow + ";", "L" + destination + ";");
+    }
+    
+    static {
+        // Inspirations Compatibility Shadowing
+        addClassShadow(
+            "com/charles445/simpledifficulty/compat/shadow/InspirationsShadow$ICauldronRecipe", 
+            "knightminer/inspirations/library/recipe/cauldron/ICauldronRecipe"
+        );
+        addClassShadow(
+            "com/charles445/simpledifficulty/compat/shadow/InspirationsShadow$ICauldronRecipe$CauldronState", 
+            "knightminer/inspirations/library/recipe/cauldron/ICauldronRecipe$CauldronState"
+        );
+    }
 }
