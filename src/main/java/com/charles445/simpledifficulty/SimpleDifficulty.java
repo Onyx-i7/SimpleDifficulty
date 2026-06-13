a9e208ab92e312170feb6ca8f4d4c148f73d03a3
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-12 23:21:56 -0400

    Update SimpleDifficulty.java

--- src/main/java/com/charles445/simpledifficulty/SimpleDifficulty.java
+++ src/main/java/com/charles445/simpledifficulty/SimpleDifficulty.java
@@ -26,7 +26,7 @@ public class SimpleDifficulty
 {
 	public static final String MODID = "simpledifficulty";
 	public static final String NAME = "SimpleDifficulty";
-	public static final String VERSION = "0.7.6";
+	public static final String VERSION = "0.7.7";
 	
 	@Mod.Instance(SimpleDifficulty.MODID)
 	public static SimpleDifficulty instance;
