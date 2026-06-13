ee9b995ba6595465c6e8bfac249684131e60f68e
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-12 21:25:51 -0400

    Optimize ParticleHeater constructor and methods
    Refactored ParticleHeater constructor to use inherited random instance for particle age initialization.

--- src/main/java/com/charles445/simpledifficulty/client/particle/ParticleHeater.java
+++ src/main/java/com/charles445/simpledifficulty/client/particle/ParticleHeater.java
@@ -2,25 +2,22 @@ package com.charles445.simpledifficulty.client.particle;
 
 import net.minecraft.world.World;
 
-public class ParticleHeater extends ParticleTemperature
-{
-	public ParticleHeater(World world, double xPos, double yPos, double zPos, double motionX, double motionY, double motionZ)
-	{
-		super(world, xPos, yPos, zPos, motionX, motionY, motionZ);
-		
-		this.particleAge = world.rand.nextInt(2);
-	}
+public class ParticleHeater extends ParticleTemperature {
 
-	@Override
-	int getFrameMax()
-	{
-		return 8;
-	}
+    public ParticleHeater(World world, double xPos, double yPos, double zPos, double motionX, double motionY, double motionZ) {
+        super(world, xPos, yPos, zPos, motionX, motionY, motionZ);
+        
+        // Optimized: Use the internal inherited random instance for particle initialization consistency
+        this.particleAge = this.rand.nextInt(2);
+    }
 
-	@Override
-	int getParticleTextureY()
-	{
-		return 0;
-	}
-	
+    @Override
+    int getFrameMax() {
+        return 8;
+    }
+
+    @Override
+    int getParticleTextureY() {
+        return 0;
+    }
 }
