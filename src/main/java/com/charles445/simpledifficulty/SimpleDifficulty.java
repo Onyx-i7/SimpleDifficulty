6276c93520e28cf031b8b05d6db18b7c1749ba44
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-24 11:18:42 -0400

    Bump version from 0.8.0 to 0.8.1

--- src/main/java/com/charles445/simpledifficulty/SimpleDifficulty.java
+++ src/main/java/com/charles445/simpledifficulty/SimpleDifficulty.java
@@ -26,7 +26,7 @@ public class SimpleDifficulty
 {
 	public static final String MODID = "simpledifficulty";
 	public static final String NAME = "SimpleDifficulty";
-	public static final String VERSION = "0.8.0";
+	public static final String VERSION = "0.8.1";
 	
 	@Mod.Instance(SimpleDifficulty.MODID)
 	public static SimpleDifficulty instance;
